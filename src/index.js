e3b169892dada0ea0d728f302fd4cbd1700d7c84
Anna Miklukha <miklukha.anna@ukr.net>
2022-04-17 21:33:59 +0200

    update task

--- src/index.js
+++ src/index.js
@@ -6,23 +6,16 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import cardImage from './templates/image-card.hbs';
 
+const debounce = require('lodash.debounce');
+
 const axios = require('axios');
-
-let gallery = new SimpleLightbox('.gallery a');
-
-const options = {
-  root: null,
-  rootMargin: '300px',
-  threshold: 1,
-};
-
-const observer = new IntersectionObserver(updateList, options);
 
 let page = 1;
 const searchForm = document.querySelector('form');
 const galleryRef = document.querySelector('.gallery');
-const sentinel = document.querySelector('.sentinel');
 const cards = document.getElementsByClassName('gallery-link');
+
+window.addEventListener('scroll', debounce(onScroll, 200));
 
 let input;
 
@@ -31,7 +24,7 @@ searchForm.addEventListener('submit', e => {
   input = e.currentTarget.searchQuery.value.trim();
   galleryRef.innerHTML = '';
   page = 1;
-  observer.unobserve(sentinel);
+
   if (input) {
     axiosImg(input, page);
   }
@@ -44,38 +37,49 @@ async function axiosImg(name, page) {
       `https://pixabay.com/api/?key=${key}&q=${name}&page=${page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`,
     );
 
+    console.log(response);
+
     if (response.data.hits.length === 0) {
       Notify.failure('Sorry, there are no images matching your search query. Please try again.');
       return;
     }
+
+    if (response.data.totalHits === cards.length) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+      return;
+    }
+
     Notify.success(`Hooray! We found ${response.data.totalHits} totalHits images.`); // щоб було один раз
     renderGallery(response.data.hits);
-
-    const a = cards[cards.length - 1].getBoundingClientRect();
-    console.log(a);
-
-    observer.observe(sentinel);
   } catch (error) {
     console.error(error);
   }
 }
-
-window.addEventListener('scroll', () => {
-  console.log(window.scrollY);
-});
 
 function renderGallery(images) {
   const markupGallery = cardImage({ images });
   galleryRef.insertAdjacentHTML('beforeend', markupGallery);
 }
 
-function updateList(entries) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function onScroll() {
+  if (cards[cards.length - 1]) {
+    let visible = isInViewport(cards[cards.length - 1]);
+
+    if (visible) {
       page += 1;
       axiosImg(input, page);
     }
-  });
+  }
 }
 
 // У відповіді бекенд повертає властивість totalHits - загальна кількість зображень, які відповідають критерію пошуку (для безкоштовного акаунту). Якщо користувач дійшов до кінця колекції, ховай кнопку і виводь повідомлення з текстом "We're sorry, but you've reached the end of search results.".
