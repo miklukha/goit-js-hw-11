d015dbdf32ea1541422ca1bf56a5475f1bcaa775
Anna Miklukha <miklukha.anna@ukr.net>
2022-04-18 09:07:57 +0200

    update task

--- src/index.js
+++ src/index.js
@@ -1,59 +1,47 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import cardImage from './templates/image-card.hbs';
-import { getRefs } from './js/gets.refs';
+
+import { getRefs } from './js/gets-refs';
+import { PhotoApiService } from './js/photo-service';
 
 const refs = getRefs();
+const photoApiService = new PhotoApiService();
 
 const debounce = require('lodash.debounce');
-const axios = require('axios');
-
-const KEY = '26815129-636df5f0482082ec4ff5cd1a9';
-
-let page = 1;
-const searchForm = document.querySelector('form');
-const galleryRef = document.querySelector('.gallery');
-const cards = document.getElementsByClassName('gallery-link');
 
 window.addEventListener('scroll', debounce(onScroll, 200));
-
-let input;
 
 refs.searchForm.addEventListener('submit', e => {
   e.preventDefault();
 
-  input = e.currentTarget.searchQuery.value.trim();
-  refs.gallery.innerHTML = '';
-  page = 1;
+  photoApiService.query = e.currentTarget.searchQuery.value.trim();
 
-  if (input) {
-    axiosImg(input, page);
+  if (photoApiService.query === '') {
+    return;
   }
+
+  clearGallery();
+  photoApiService.resetPage();
+  checkRequest();
 });
 
-async function axiosImg(name, page) {
-  try {
-    const response = await axios.get(
-      `https://pixabay.com/api/?key=${KEY}&q=${name}&page=${page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`,
-    );
+async function checkRequest() {
+  const response = await photoApiService.axios();
 
-    console.log(response);
-
-    if (response.data.hits.length === 0) {
-      Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-      return;
-    }
-
-    if (response.data.totalHits === refs.renderedCards.length) {
-      Notify.info("We're sorry, but you've reached the end of search results.");
-      return;
-    }
-
-    Notify.success(`Hooray! We found ${response.data.totalHits} totalHits images.`); // щоб було один раз
-    renderGallery(response.data.hits);
-  } catch (error) {
-    console.error(error);
+  if (response.hits.length === 0) {
+    Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    return;
   }
+
+  Notify.success(`Hooray! We found ${response.totalHits} totalHits images.`);
+
+  axiosImg();
+}
+
+async function axiosImg() {
+  const response = await photoApiService.axios();
+  renderGallery(response.hits);
 }
 
 function renderGallery(images) {
@@ -76,10 +64,14 @@ function onScroll() {
     let visible = isInViewport(refs.renderedCards[refs.renderedCards.length - 1]);
 
     if (visible) {
-      page += 1;
-      axiosImg(input, page);
+      photoApiService.resetPage();
+      axiosImg();
     }
   }
+}
+
+function clearGallery() {
+  refs.gallery.innerHTML = '';
 }
 
 // У відповіді бекенд повертає властивість totalHits - загальна кількість зображень, які відповідають критерію пошуку (для безкоштовного акаунту). Якщо користувач дійшов до кінця колекції, ховай кнопку і виводь повідомлення з текстом "We're sorry, but you've reached the end of search results.".
---
+++ src/js/photo-service.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+
+const API_KEY = '26815129-636df5f0482082ec4ff5cd1a9';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export class PhotoApiService {
+  constructor() {
+    (this.searchQuery = ''), (this.page = 1);
+  }
+
+  async axios() {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&page=${this.page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`,
+      );
+
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  incrementPage() {
+    this.pae += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
